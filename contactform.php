607f9b8b9cd424cbb95355bc1a082b6825f15549
NickLojas <nick.lojas@mail.utoronto.ca>
2018-11-11 16:16:41 -0500

    Added contact form functionality using php

---
+++ contactform.php
@@ -0,0 +1,20 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$rating = $_POST['rating'];
+
+	$mailTo = "nick.lojas@hotmail.com";
+	$subject = "Website form submit";
+	$headers = "From: ".$email;
+	$txt = "You have received an email from ".$name.".\n\n".$message.".\n\nWith Rating: ".$rating;
+
+
+	mail($mailTo, $subject, $txt, $headers);
+
+	header("Location: contact.html?mailsend");
+}
+
+ ?>
